f6f0ff8ca3a375ce21a3fadb319ada5c2c507371
Nikhil-Kanojiya01 <Nikhil.Kanojiya@intmavens.com>
2025-11-13 17:47:59 +0530

    Updated App.css styling and modified Testimonials component

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -4,32 +4,28 @@ import Carousel from './Carousel'
 
 const Testimonials = () => {
 	const renderTestimonial = (testimonial) => (
-		<div className="testimonial-card">
-			<div className="testimonial-card__content">
-				<div className="testimonial-card__quote">
-					<i className="fas fa-quote-left testimonial-card__quote-icon"></i>
-					<p className="testimonial-card__text">"{testimonial.testimonial}"</p>
-				</div>
-				<div className="testimonial-card__author">
-					<div className="testimonial-card__author-info">
-						<h4 className="testimonial-card__name">{testimonial.name}</h4>
-						<p className="testimonial-card__position">{testimonial.position}</p>
-						{testimonial.company && (
-							<a 
-								href={testimonial.website} 
-								target="_blank" 
-								rel="noopener noreferrer"
-								className="testimonial-card__company"
-							>
-								{testimonial.company}
-							</a>
-						)}
-					</div>
-					<div className="testimonial-card__rating">
-						{[...Array(5)].map((_, i) => (
-							<i key={i} className="fas fa-star testimonial-card__star"></i>
-						))}
-					</div>
+		<div className="testimonial-simple">
+			<div className="testimonial-simple__quote">
+				<i className="fas fa-quote-left testimonial-simple__quote-icon"></i>
+				<p className="testimonial-simple__text">"{testimonial.testimonial}"</p>
+			</div>
+			<div className="testimonial-simple__author">
+				<h4 className="testimonial-simple__name">{testimonial.name}</h4>
+				<p className="testimonial-simple__position">{testimonial.position}</p>
+				{testimonial.company && (
+					<a 
+						href={testimonial.website} 
+						target="_blank" 
+						rel="noopener noreferrer"
+						className="testimonial-simple__company"
+					>
+						{testimonial.company}
+					</a>
+				)}
+				<div className="testimonial-simple__rating">
+					{[...Array(5)].map((_, i) => (
+						<i key={i} className="fas fa-star testimonial-simple__star"></i>
+					))}
 				</div>
 			</div>
 		</div>
